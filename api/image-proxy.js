27b87cd2b56d2c2f56d2f4a1fc66ac1dff4110e4
nateykliu <nathanykliu@gmail.com>
2023-11-30 14:40:03 -0800

    routes

--- api/image-proxy.js
+++ api/image-proxy.js
@@ -8,6 +8,20 @@ export default async function (req, res) {
     }
 
     try {
+        // fetch image headers first to check the size
+        const headResponse = await fetch(imageUrl, { method: 'HEAD' });
+        if (!headResponse.ok) {
+            throw new Error(`Failed to fetch image headers: ${headResponse.statusText}`);
+        }
+
+        // check image size from the headers
+        const contentLength = headResponse.headers.get('content-length');
+        if (contentLength && parseInt(contentLength, 10) > 4.5 * 1024 * 1024) { // 4.5MB
+            res.redirect(imageUrl);
+            return;
+        }
+
+        // if size is within limits, proceed to fetch the image
         const response = await fetch(imageUrl);
         if (!response.ok) {
             throw new Error(`Failed to fetch image: ${response.statusText}`);
@@ -15,6 +29,7 @@ export default async function (req, res) {
         const arrayBuffer = await response.arrayBuffer();
         const imageBuffer = Buffer.from(arrayBuffer);
 
+        // determine the content type
         let contentType = 'image/jpg'; // default to jpg
         if (imageUrl.endsWith('.png')) {
             contentType = 'image/png';
@@ -26,12 +41,11 @@ export default async function (req, res) {
             contentType = 'image/jpeg';
         }
         
+        // send the image back
         res.setHeader('Content-Type', contentType);
         res.send(imageBuffer);
     } catch (error) {
-        res.redirect(imageUrl);
         console.error(`Error fetching ${imageUrl}:`, error);
         res.status(500).send('Error fetching image');
     }
 }
-
